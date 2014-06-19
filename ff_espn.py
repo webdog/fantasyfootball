5ea0cc15bb95080a4432015ad3cd2bc3cced4cd1
cmathew.weber@gmail.com <weberc@AACCOFF5832.fortisclearingus.com>
2014-06-19 16:12:26 -0500

    ff_espn.py

--- ff_espn.py
+++ ff_espn.py
@@ -2,28 +2,78 @@
 import requests
 import xml.etree.ElementTree as ET
 
-auth = {"apikey" : "api_str"}
-base_url = "http://api.espn.com/v1/sports"
+def new_query():
+	print "This is the data collection app that takes data from the ESPN api, and returns data to json format.\n"
+	print "Please select the sports data you want:\n"
+	x = raw_input("1. NHL\n2. NBA\n3. MLB\n4."i)
+	input_list = int([1, 2, 3, 4])
+	if x not in input_list:
+		print "Invalid selection. Exiting"
+		exit()
+	else:
+		print "Passing request to API. Standby"
 
-resp = requests.get(base_url, params=auth)
-rc = int(resp.status_code)
+def gather_info(x):
 
-sc = [200, 400, 401, 403, 404, 500, 504]
-sc_r = ['Successful', 'Bad Request', 'Unuthorized', 'Account Over Quota', 'Not Found', 'Internal Server Error', 'TimeOut']
+	auth = {"apikey" : "api_str"}
+	base_url = "http://api.espn.com/v1/sports"
 
-if rc == sc[0]:
-	print sc_r[0]
-elif rc == sc[1]:
-	print "Request failed. Reason:" sc_r[1]
-elif rc == sc[2]:
-	print "Request failed. Reason:" sc_r[2]
-elif rc == sc[3]:
-	print "Request failed. Reason:" sc_r[3]
-elif rc == sc[4]:
-	print "Request failed. Reason:" sc_r[4]
-elif rc == sc[5]:
-	print "Request failed. Reason:" sc_r[5]
-elif rc == sc[6]:
-	print "Request failed. Reason:" sc_r[6]
-elif rc not in sc:
-	print "Request failed. Reason:" rc
+	resp = requests.get(base_url, params=auth)
+	rc = int(resp.status_code)
+
+	#HTTP status codes
+	sc = int([
+			200, 
+			400, 
+			401, 
+			403, 
+			404, 
+			500, 
+			504
+			])
+	#Values of HTTP status codes, matched by position of sc
+	sc_r = [
+			'Successful', 
+			'Bad Request', 
+			'Unuthorized', 
+			'Account Over Quota', 
+			'Not Found', 
+			'Internal Server Error', 
+			'TimeOut'i
+			]
+	
+	#Check if return code (rc) is 200 (First position of sc). If so, continue.
+	if rc == sc[0]:
+		print sc_r[0]
+	
+	#If API request fails the listed elifs will go through the list of common HTTP responses and print out the response.
+	elif rc == sc[1]:
+		print "Request failed. Reason:" sc_r[1]
+		exit()
+	elif rc == sc[2]:
+		print "Request failed. Reason:" sc_r[2]
+		exit()
+	elif rc == sc[3]:
+		print "Request failed. Reason:" sc_r[3]
+		exit()
+	elif rc == sc[4]:
+		print "Request failed. Reason:" sc_r[4]
+		exit()
+	elif rc == sc[5]:
+		print "Request failed. Reason:" sc_r[5]
+		exit()
+	elif rc == sc[6]:
+		print "Request failed. Reason:" sc_r[6]
+		exit()
+	elif rc not in sc:
+		print "Request failed. Reason:" rc
+		exit()
+	else:
+		"Unexpected Response. Exiting"
+		exit()
+
+
+if name == "__main__":
+	new_query()
+	gather_info(new_query(x))
+
