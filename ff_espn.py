b9b3c9e13bd3f41744eb28b57a78e38b20f35af4
U-ChristianWeber\Christian Weber <Christian Weber@ChristianWeber.(none)>
2014-06-19 22:33:00 -0500

    ff_espn.py

--- ff_espn.py
+++ ff_espn.py
@@ -5,9 +5,9 @@ import xml.etree.ElementTree as ET
 def new_query():
 	print "This is the data collection app that takes data from the ESPN api, and returns data to json format.\n"
 	print "Please select the sports data you want:\n"
-	x = raw_input("1. NHL\n2. NBA\n3. MLB\n4."i)
-	input_list = int([1, 2, 3, 4])
-	if x not in input_list:
+	x = int(raw_input("1. NHL\n2. NBA\n3. MLB\n4. NFL\n>"))
+	valid_input = [int(1), int(2), int(3), int(4)]
+	if x not in valid_input:
 		print "Invalid selection. Exiting"
 		exit()
 	else:
@@ -32,48 +32,46 @@ def gather_info(x):
 			504
 			])
 	#Values of HTTP status codes, matched by position of sc
-	sc_r = [
+	sc_r = str([
 			'Successful', 
 			'Bad Request', 
 			'Unuthorized', 
 			'Account Over Quota', 
 			'Not Found', 
 			'Internal Server Error', 
-			'TimeOut'i
-			]
+			'TimeOut'
+			])
 	
 	#Check if return code (rc) is 200 (First position of sc). If so, continue.
 	if rc == sc[0]:
 		print sc_r[0]
-	
 	#If API request fails the listed elifs will go through the list of common HTTP responses and print out the response.
 	elif rc == sc[1]:
-		print "Request failed. Reason:" sc_r[1]
+		print "Request failed. Reason: %s" % sc_r[1]
 		exit()
 	elif rc == sc[2]:
-		print "Request failed. Reason:" sc_r[2]
+		print "Request failed. Reason: %s" % sc_r[2]
 		exit()
 	elif rc == sc[3]:
-		print "Request failed. Reason:" sc_r[3]
+		print "Request failed. Reason: %s" % sc_r[3]
 		exit()
 	elif rc == sc[4]:
-		print "Request failed. Reason:" sc_r[4]
+		print "Request failed. Reason: %s" % sc_r[4]
 		exit()
 	elif rc == sc[5]:
-		print "Request failed. Reason:" sc_r[5]
+		print "Request failed. Reason: %s" % sc_r[5]
 		exit()
 	elif rc == sc[6]:
-		print "Request failed. Reason:" sc_r[6]
+		print "Request failed. Reason: %s" % sc_r[6]
 		exit()
 	elif rc not in sc:
-		print "Request failed. Reason:" rc
+		print "Request failed. Reason: %s" % rc
 		exit()
 	else:
 		"Unexpected Response. Exiting"
 		exit()
 
 
-if name == "__main__":
+if __name__ == "__main__":
 	new_query()
-	gather_info(new_query(x))
-
+	
